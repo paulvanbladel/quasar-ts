dc14512334bb9b7db42d5fe2b7513dfb83d65742
paul van bladel <paul.vanbladel@gmail.com>
2017-03-30 10:25:27 +0200

    webpack test config

--- build/webpack.test.conf.js
+++ build/webpack.test.conf.js
@@ -7,45 +7,25 @@ var
   HtmlWebpackPlugin = require('html-webpack-plugin'),
   FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
-// add hot-reload related code to entry chunks
-Object.keys(baseWebpackConfig.entry).forEach(function (name) {
-  baseWebpackConfig.entry[name] = ['./build/hot-reload'].concat(baseWebpackConfig.entry[name])
-})
 
 var webpackConfig =  merge(baseWebpackConfig, {
-  // eval-source-map is faster for development
-  devtool: '#cheap-module-eval-source-map',
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true
-  },
+
   module: {
     rules: cssUtils.styleRules({
       sourceMap: config.dev.cssSourceMap,
       postcss: true
     })
   },
+  devtool: '#inline-source-map',
    plugins: [
-    //  new HtmlWebpackPlugin({
-    //   filename: 'index.html',
-    //   template: 'src/index.html',
-    //   inject: true
-    // })
-    ]
-  //   new webpack.HotModuleReplacementPlugin(),
-  //   new webpack.NoEmitOnErrorsPlugin(),
-  //   new HtmlWebpackPlugin({
-  //     filename: 'index.html',
-  //     template: 'src/index.html',
-  //     inject: true
-  //   }),
-  //   new FriendlyErrorsPlugin({
-  //     clearConsole: config.dev.clearConsoleOnRebuild
-  //   })
-  // ],
-  // performance: {
-  //   hints: false
-  // }
+    new webpack.DefinePlugin({
+      'process.env': require('../config/test.env')
+    }),
+    new webpack.SourceMapDevToolPlugin({
+      filename: null, // if no value is provided the sourcemap is inlined
+      test: /\.(ts|js)($|\?)/i
+    })
+  ]
 })
 // no need for app entry during tests
 delete webpackConfig.entry
